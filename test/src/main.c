94b2aaa96fcbe200e548a70ecfc6684753b079ec
Andrei Shkarin <cupmathii@gmail.com>
2014-08-23 19:59:03 +0200

    changes

--- test/src/main.c
+++ test/src/main.c
@@ -12,10 +12,14 @@ int main(int n_args, char *argv[])
     UfoConfig *config = ufo_config_new();
     UfoGraph *ufo_task_graph = ufo_task_graph_new();
     UfoScheduler *ufo_scheduler = ufo_scheduler_new (config, NULL);
-    //g_object_set (ufo_scheduler, "enable-tracing", TRUE, NULL);
+    g_object_set (ufo_scheduler, "enable-tracing", TRUE, NULL);
     UfoPluginManager *manager = ufo_plugin_manager_new (config);
     UfoNode *reader = UFO_NODE (ufo_plugin_manager_get_task (manager, "reader", NULL));
     UfoNode *writer = UFO_NODE (ufo_plugin_manager_get_task (manager, "writer", NULL));
+    UfoNode *generator = UFO_NODE (ufo_plugin_manager_get_task (manager, "generate", NULL));
+    UfoNode *null = UFO_NODE (ufo_plugin_manager_get_task (manager, "null", NULL));
+    g_object_set (generator, "width", 729, "height", 729, "bitdepth", 16, NULL);
+
     UfoNode *ir = UFO_NODE (ufo_plugin_manager_get_task (manager, "ir", &error));
     if (error){
         printf("\nError: Run was unsuccessful: %s\n", error->message);
@@ -71,7 +75,7 @@ int main(int n_args, char *argv[])
 
     g_object_set (sart,
                   "relaxation-factor", 0.25,
-                  "max-iterations", 300,
+                  "max-iterations", 10,
                   NULL);
 
     gpointer asdpocs = ufo_plugin_manager_get_plugin (manager,
@@ -104,12 +108,14 @@ int main(int n_args, char *argv[])
     ufo_ir_prior_knowledge_set_pointer (prior, "image-sparsity", sparsity);
 
     g_object_set (ir,
-                  "method", sart,
+                  "method", asdpocs,
                   "geometry", geometry,
                   "projector", projector,
                   "prior-knowledge", prior,
                   NULL);
 
+    //ufo_graph_connect_nodes (ufo_task_graph, reader, null, NULL);
+    //ufo_graph_connect_nodes (ufo_task_graph, generator, writer, NULL);
     ufo_graph_connect_nodes (ufo_task_graph, reader, ir, NULL);
     ufo_graph_connect_nodes (ufo_task_graph, ir, writer, NULL);
     ufo_base_scheduler_run (ufo_scheduler, UFO_TASK_GRAPH (ufo_task_graph), &error);
