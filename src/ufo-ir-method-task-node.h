d056a600b63a2e3b08b023e62394022f4df0c9f8
buldo <froooks@gmail.com>
2015-06-02 10:44:27 +0200

    Small changes in macros names

--- src/ufo-ir-method-task-node.h
+++ src/ufo-ir-method-task-node.h
@@ -26,9 +26,9 @@ G_BEGIN_DECLS
 
 #define UFO_IR_TYPE_METHOD_TASK_NODE             (ufo_ir_method_task_node_get_type())
 #define UFO_IR_METHOD_TASK_NODE(obj)             (G_TYPE_CHECK_INSTANCE_CAST((obj), UFO_IR_TYPE_METHOD_TASK_NODE, UfoIrMethodTaskNode))
-#define UFO_IS_IR_METHOD_TASK_NODE(obj)          (G_TYPE_CHECK_INSTANCE_TYPE((obj), UFO_IR_TYPE_METHOD_TASK_NODE))
+#define UFO_IR_IS_METHOD_TASK_NODE(obj)          (G_TYPE_CHECK_INSTANCE_TYPE((obj), UFO_IR_TYPE_METHOD_TASK_NODE))
 #define UFO_IR_METHOD_TASK_NODE_CLASS(klass)     (G_TYPE_CHECK_CLASS_CAST((klass), UFO_IR_TYPE_METHOD_TASK_NODE, UfoIrMethodTaskNodeClass))
-#define UFO_IS_IR_METHOD_TASK_NODE_CLASS(klass)  (G_TYPE_CHECK_CLASS_TYPE((klass), UFO_IR_TYPE_METHOD_TASK_NODE))
+#define UFO_IR_IS_METHOD_TASK_NODE_CLASS(klass)  (G_TYPE_CHECK_CLASS_TYPE((klass), UFO_IR_TYPE_METHOD_TASK_NODE))
 #define UFO_IR_METHOD_TASK_NODE_GET_CLASS(obj)   (G_TYPE_INSTANCE_GET_CLASS((obj), UFO_IR_TYPE_METHOD_TASK_NODE, UfoIrMethodTaskNodeClass))
 
 typedef struct _UfoIrMethodTaskNode           UfoIrMethodTaskNode;
